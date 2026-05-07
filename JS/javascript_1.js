564ac93feea7f7a8983114dfca0fb1465825fae2
Felipe <mmillanpelaez@danielcastelao.org>
2026-05-07 12:11:43 +0200

    Add initial HTML structure for boletin_xpath with product listings

---
+++ JS/javascript_1.js
@@ -0,0 +1,18 @@
+/* Exercicio 1: variables e tipos
+● Declara variables para gardar: o teu nome, idade, se estás matriculado en DAM ou
+ASIR (booleano).
+● Mostra todo con console.log. */
+
+let name = "Felipe";
+let age = 25;
+let isEnrolledInDAM = true;
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Is enrolled in DAM: " + isEnrolledInDAM);
+
+/* Exercicio 2: condicións
+● Pide unha nota co prompt.
+● Converte a número (Number(...)).
+● Mostra por consola se é suspenso, aprobado, notable ou sobresaínte. */
+let grade = Number("Introduce tu nota: ");
